df0bfe81cbabe1684364da7ecdc9b884e454b4c0
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-10-29 15:46:05 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,3 +1,4 @@
+#include "pch.h"
 #include <iostream>
 
 using namespace std;
@@ -9,10 +10,39 @@ int main()
 	cout << "Введите числа A, B и C " << endl;
 	cin >> a >> b >> c;
 
-	max = a > b ? a : b;
-	max1 = c > b ? c : b;
-	sum = max + max1;
-
+	if (a >= b && a >= c)
+	{
+		if (b > c)
+		{
+			sum = a + b;
+		}
+		else
+		{
+			sum = a + c;
+		}
+	}
+	else if (b >= a && b >= c)
+	{
+		if (c > a)
+		{
+			sum = b + c;
+		}
+		else
+		{
+			sum = b + a;
+		}
+	}
+	else if (c >= a && c >= b)
+	{
+		if (a > b)
+		{
+			sum = c + a;
+		}
+		else
+		{
+			sum = c + b;
+		}
+	}
 	cout << "Сумма наибольших двух чисел равна " << sum << endl;
 
 	system("pause");
