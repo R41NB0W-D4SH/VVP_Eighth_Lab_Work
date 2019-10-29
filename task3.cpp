1dda90e00f804fac8a62a641c1c3966ae5db1de3
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-10-29 15:49:04 +0300

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -9,15 +9,22 @@ int main()
 	double a, b, c, min;
 	
 	cout << "Введите координа трех точек. Сначала A, потом B, затем C" << endl;
-	cin >> a >> b >> c;
+	cin >> ax >> bx >> cx;
+	cin >> ay >> by >> cy;
 
-	if (abs(a - c) < abs(a - b))
+	double cd, bd;
+	cd = sqrt(pow(cx - ax, 2) + pow(cy - ay, 2));
+	bd = sqrt(pow(bx - ax, 2) + pow(by - ay, 2));
+
+
+
+	if (bd < cd )
 	{
-		cout << "Ближайшая точка к A это точка C равная " << c << " и ее расстояние до A равно " << abs(a - c) << endl;
+		cout << "Ближайшая точка к A это точка C равная " << c << " и ее расстояние до A равно " << cd << endl;
 	}
 	else
 	{
-		cout << "Ближайшая точка к A это точка B равная " << b << " и ее расстояние до A равно " << abs(a - b) << endl;
+		cout << "Ближайшая точка к A это точка B равная " << b << " и ее расстояние до A равно " << bd << endl;
 	}
 
 	system("pause");
